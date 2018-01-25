0f479639f17ee2d2aca1acf20d427c1d2a379521
kAoi97 <mailkaoi97@gmail.com>
2018-01-25 15:04:00 -0500

    Add images for project section

--- application/views/landing/projects.php
+++ application/views/landing/projects.php
@@ -2,10 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!-- Section -->
 									<section class="carousel accent4">
+										<header class="major">
+											<a name="about"></a>
+											<h2>Mientras tanto en el hackerspace...</h2>
+											<p>A continuacion encontraras un listado de nuestros proyectos, actividades, y eventos llevados a cabo por la comundad de Rootdevel Hackerspace</p>
+										</header>
 										<article>
-											<div class="image"><img src="<?= base_url() ?>application/media/images/pic04.jpg" alt="" data-position="center" /></div>
+											<div class="image"><img src="<?= base_url() ?>application/media/images/CNC-Animation.gif" alt="" data-position="center" /></div>
 											<div class="content">
-												<h3>Magna tempus</h3>
+												<h3>Protitopo digital de una CNC (Control Numérico Computarizado).</h3>
 												<p>Adipiscing ullamcorper semper ut amet ac risus. Lorem sapien ut odio odio nunc. Ac adipiscing nibh porttitor erat risus justo adipiscing adipiscing amet placerat.</p>
 											</div>
 										</article>
